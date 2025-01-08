4545eb019ba276515716e1d3e26d4747328e5bca
krestwastaken <martynskelvin@gmail.com>
2025-01-08 20:42:21 +0100

    teething stage 2

--- src/Components/TaskDisplay.jsx
+++ src/Components/TaskDisplay.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskList from './TaskList'
 
 export default function TaskDisplay() {
+  const [ todos, setTodos ] = useState([])
   return (
     <div>
-      
+      <TaskList />
     </div>
   )
 }
