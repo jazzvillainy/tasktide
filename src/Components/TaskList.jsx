821e91361d5e4f00282a46e8bdc9784800ed8b8f
krestwastaken <martynskelvin@gmail.com>
2025-01-12 00:01:40 +0100

    mapping 2

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -4,7 +4,9 @@ import TaskItem from './TaskItem'
 export default function TaskList({ tasks }) {
   return (
     <div>
-        <TaskItem/>
+        {tasks.map((item) => (
+            <TaskItem item={item} />
+        ))}
     </div>
   )
 }
